1899e57a7bfd7da41e35dc7b5d0ad45a3a9a352f
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-04-09 13:46:26 +0800

    django-rest-framework

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,15 @@
+from .models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework import generics
+
+'''
+Using generic class-based views
+'''
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer

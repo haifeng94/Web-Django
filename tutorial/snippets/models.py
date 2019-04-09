f28bd73c342580d4f4b1f3369ce69741e17635f7
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-04-09 16:08:51 +0800

    django-rest-framework

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 #pygments项目中会用到，提供代码高亮功能
 #提取出了pygments支持的所有语言的词法分析程序
@@ -17,6 +19,20 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False) #是否显示行号
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100) #语言
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100) #风格
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ('created', )
+
+    def save(self, *args, **kwargs):
+        """
+            Use the `pygments` library to create a highlighted HTML
+            representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)

fbb2bbf553d8465ebdf6bcfed58ab7af8f7738d8
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-04-09 16:11:47 +0800

    django-rest-framework

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,9 @@
 from .models import Snippet
 from .serializers import SnippetSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 '''
 Using generic class-based views
@@ -10,6 +13,19 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

2fee7c2594fe039fae6203865d005a5115139afc
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-04-10 16:24:11 +0800

    django-rest-framework

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    path('schema/', schema_view),
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
 ]
